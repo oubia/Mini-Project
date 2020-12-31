1a134803a36587f68425ad2e38afea84895c34f3
HananeJagour <hanane2000hanouna@gmail.com>
2021-01-01 00:06:26 +0100

    v1_m_p

---
+++ mini_projet_école_privé/src/com/company/Directeur.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.sql.*;
+public class Directeur {
+    Connection con;
+    Statement stm;
+    ResultSet rst;
+    private double log_d=1111;
+    private double passwrd_d=1111;
+
+}
